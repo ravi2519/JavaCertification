3358a7d9c9336512367c88417c24c3676b7545bc
Ravi Ranjan <ravi2519@gmail.com>
2020-10-07 01:52:39 +0530

    inheritance and overriding

--- basics/JavaInheritance.java
+++ basics/JavaInheritance.java
@@ -1,9 +1,5 @@
 package basics;
 
-public class JavaInheritance {
-    
-}
-
 // 1.
 // A doesn't have a no argument constructor.
 // So when B comes into picture, java will try
@@ -13,9 +9,71 @@ public class JavaInheritance {
 // this will end up with a compile time error.
 class A {
     A(int n){}
+
+    void m1 () {
+        System.out.println("Parent A");
+    }
 }
 
-class B extends A {
+// UNCOMMENT BELOW
+// class B extends A {
+// }
+
+// END OF 1
+
+// 2.
+// Adding a final to a overriding method is still valid
+class C extends A {
+    C(int n){
+        super(n);
+    }
+
+    public final void m1() { // <=== NO PROBLEM HERE
+        System.out.println("Child C");
+    }
+}
+
+// END OF 2
+
+// 3.
+// A doesn't have a no argument constructor.
+// So in class D, Java will add a call to super()
+// as the first statement to the no argument 
+// constructor. This will fail
+// UNCOMMENT BELOW
+// class D extends A {
+//     D(){}
+// }
+
+// END OF 3
+
+// 4.
+// A doesn't have a no argument constructor.
+// So in class E, Java will add a call to super()
+// as the first statement to the single argument 
+// constructor. This will fail.
+// TO correct this we can add a call to super(n)
+// UNCOMMENT BELOW
+// class E extends A {
+//     E(int n){System.out.println("E");}
+// }
+
+// END OF 4
+
+public class JavaInheritance {
+
+    public static void main(String[] args) {
+        
+        System.out.println("-----------------------2-----------------");
+        A a = new C(1);
+        a.m1();
+        System.out.println("-----------------------2-----------------");
+
+    }
 }
 
 
+
+
+
+    
---
+++ basics/JavaMethodOverriding.java
@@ -0,0 +1,24 @@
+package basics;
+
+import java.util.List;
+
+/**
+ * JavaMethodOverriding
+ */
+public class JavaMethodOverriding {
+
+    
+}
+
+// 1.
+// Not a valid override
+class AA {
+    protected List<String> hairy(int p) { return null; }
+}
+
+class BB extends AA{
+    // UNCOMMENT THE CODE
+    // protected Object hairy(int p) { return null;}
+}
+
+// END OF 1
