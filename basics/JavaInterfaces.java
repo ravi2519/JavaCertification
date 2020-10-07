c78a7daacb7e433beb70718fdecc5daf2d4c1259
Ravi Ranjan <ravi2519@gmail.com>
2020-10-08 00:19:02 +0530

    Interfaces

---
+++ basics/JavaInterfaces.java
@@ -0,0 +1,77 @@
+package basics;
+
+
+
+// 1.
+// Return type of fly() in CanFly and HasWings
+// doesn't follow the overriding rules 
+interface CanFly {
+    int fly();
+
+    String fly(int distance);
+}
+
+interface HasWings {
+    abstract String fly();
+
+    public abstract Object getWingSpan();
+}
+
+// UNCOMMENT BELOW
+// abstract class Falcon implements CanFly, HasWings {
+// }
+
+// END OF 1
+
+
+// 2.
+// Using instanceof
+interface Apple {
+}
+
+interface Orange {
+}
+
+class Gala implements Apple {
+}
+
+class Tangerine implements Orange {
+}
+
+final class Citrus extends Tangerine {
+}
+
+public class JavaInterfaces {
+    public static void main(String... farm) {
+        Gala g = new Gala();
+        Tangerine t = new Tangerine();
+        Citrus c = new Citrus();
+
+        // Compiler error
+        // Both t and Gala are classes, so they strictly give a 
+        // compiler error when we use instanceof with them
+        // UNCOMMENT BELOW
+        // System.out.print(t instanceof Gala);
+        
+        // perfectly fine
+        System.out.print(c instanceof Tangerine);
+        
+        // perfectly fine
+        System.out.print(g instanceof Apple);
+        
+        // This doesnot throw a compiler error
+        // the logic is that there may be a case where 
+        // class Tangerine might be extended by some 
+        // other class and that class implements Apple 
+        System.out.print(t instanceof Apple);
+
+        // Similar to above. But since CItrus is 
+        // final it can't be overridden. Thus no implementation of
+        // Apple in heirearchy. 
+        // UNCOMMENT BELOW
+        // System.out.print(c instanceof Apple);
+    }
+}
+
+// END OF 2
+
